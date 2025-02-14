c4f4694688643e96981ddff359fa9851acb3d2ed
Jesse Stuart <email@jessestuart.ca>
2025-02-13 22:28:13 -0500

    feat: apply alter column migrations

--- src/migration.rs
+++ src/migration.rs
@@ -1,4 +1,7 @@
-use sqlparser::ast::{AlterTableOperation, CreateTable, ObjectType, Statement};
+use sqlparser::ast::{
+    AlterColumnOperation, AlterTableOperation, ColumnOption, ColumnOptionDef, CreateTable,
+    GeneratedAs, ObjectType, Statement,
+};
 
 pub trait Migrate: Sized {
     fn migrate(self, other: &Self) -> Option<Self>;
@@ -112,6 +115,72 @@ fn migrate_alter_table(mut t: CreateTable, ops: &[AlterTableOperation]) -> Creat
                 drop_behavior: _,
             } => {
                 t.columns.retain(|c| c.name != *column_name);
+            }
+            AlterTableOperation::AlterColumn { column_name, op } => {
+                t.columns.iter_mut().for_each(|c| {
+                    if c.name != *column_name {
+                        return;
+                    }
+                    match op {
+                        AlterColumnOperation::SetNotNull => {
+                            c.options.push(ColumnOptionDef {
+                                name: None,
+                                option: ColumnOption::NotNull,
+                            });
+                        }
+                        AlterColumnOperation::DropNotNull => {
+                            c.options
+                                .retain(|o| !matches!(o.option, ColumnOption::NotNull));
+                        }
+                        AlterColumnOperation::SetDefault { value } => {
+                            c.options
+                                .retain(|o| !matches!(o.option, ColumnOption::Default(_)));
+                            c.options.push(ColumnOptionDef {
+                                name: None,
+                                option: ColumnOption::Default(value.clone()),
+                            });
+                        }
+                        AlterColumnOperation::DropDefault => {
+                            c.options
+                                .retain(|o| !matches!(o.option, ColumnOption::Default(_)));
+                        }
+                        AlterColumnOperation::SetDataType {
+                            data_type,
+                            using: _, // not applicable since we're not running the query
+                        } => {
+                            c.data_type = data_type.clone();
+                        }
+                        AlterColumnOperation::AddGenerated {
+                            generated_as,
+                            sequence_options,
+                        } => {
+                            c.options.retain(|o| {
+                                !matches!(
+                                    o.option,
+                                    ColumnOption::Generated {
+                                        generated_as: _,
+                                        sequence_options: _,
+                                        generation_expr: _,
+                                        generation_expr_mode: _,
+                                        generated_keyword: _
+                                    }
+                                )
+                            });
+                            c.options.push(ColumnOptionDef {
+                                name: None,
+                                option: ColumnOption::Generated {
+                                    generated_as: generated_as
+                                        .clone()
+                                        .unwrap_or(GeneratedAs::Always),
+                                    sequence_options: sequence_options.clone(),
+                                    generation_expr: None,
+                                    generation_expr_mode: None,
+                                    generated_keyword: true,
+                                },
+                            });
+                        }
+                    }
+                });
             }
             _ => todo!("handle alter table operation {:?}", op),
         }
